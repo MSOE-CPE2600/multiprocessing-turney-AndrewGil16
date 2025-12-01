10dbf037db22957dc0a732ebe6d5d6acb689fcb5
Andrew Gilpatrick <gilpatricka@msoe.edu>
2025-12-01 13:49:02 -0600

    Lab12Done

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# System Programming Lab 11 Multiprocessing
+# System Programming Lab 12
 
 
-# Notes
-for some reason more children made it less efficient. i decided to get the flu right as this was due, so it works enough
+# Overview
+This program generates a series of images using multiple processes and threads. This speeds up the rendering of each frame. Each child process renders its assigned frames in parallel using threads.
+
+
 
 # Data
-Graph of time data located in Children Process Graph.pdf
+|**Processes/Threads**| **1 Thread** | **2 Threads** | **4 Threads** | **8 Threads**|
+|                     |              |               |               |              |
+|**1 Process**        |  78.19s      |  42.98s       |  39.99s       |  28.02s      |
+|**2 Processes**      |  54.58s      |  34.05s       |  31.44s       |  23.01s      |
+|**4 Processes**      |  32.34s      |  22.80s       |  20.48s       |  15.05s      |
+|**8 Processes**      |  24.25s      |  16.54s       |  15.09s       |  15.01s      |
+
+## Result Discussion
+- Going from one thread to multiple had the biggest impact on speed but more processes per thread will always make it faster. I think this is due to the fact that we need 50 images regardless, so more "instances" of image generation will be more efficient then using multiple threads makes calculating a single image faster.
+- The "sweet spot" was 4 threads with 8 processes, and 8 threads with 4 or 8 processes
--- mandel.c
+++ mandel.c
@@ -1,201 +1,287 @@
-/// 
-//  mandel.c
-//  Based on example code found here:
-//  https://users.cs.fiu.edu/~cpoellab/teaching/cop4610_fall22/project3.html
-//
-//  Converted to use jpg instead of BMP and other minor changes
-//  
-///
+/**********************************
+* Author: Andrew Gilpatrick
+* Assignment: Lab 12
+* Date: 12/1/2025
+* CPE2600-112
+**********************************/
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <unistd.h>
 #include "jpegrw.h"
 #include <math.h>
 #include <sys/wait.h>
 
-
-// local routines
 static int iteration_to_color( int i, int max );
 static int iterations_at_point( double x, double y, int max );
-static void compute_image( imgRawImage *img, double xmin, double xmax,
-									double ymin, double ymax, int max );
+static void compute_image( imgRawImage *img, double xmin, double xmax,double ymin, double ymax, int maxIter, int numThreads );
 static void show_help();
 
 int opt;
 int number = 1; // number of child processes
 int workload = 50; // 50 images is the workload
-double scale = .2; // scale multiplier
-int main( int argc, char *argv[] )
-{
-	char c;
+double scale = 0.98; // Zoom multiplier
+double zoom_factor = 0.98; // Default zoom
+int numThreads = 1;   // default for threading
 
-	// These are the default configuration values used
-	// if no command line arguments are given.
-	const char *outfile = "mandel%d.jpg";
-	char newName[50];
-	double xcenter = 0;
-	double ycenter = 0;
-	double xscale = 4;
-	double yscale = 0; // calc later
-	int    image_width = 1000;
-	int    image_height = 1000;
-	int    max = 1000;
+// Threading structure
+typedef struct {
+    imgRawImage *img;
+    double xmin;
+    double xmax;
+    double ymin;
+    double ymax;
+    int maxIter;
+    int startRow;
+    int endRow;
+} ThreadJob;
 
-	// For each command line argument given,
-	// override the appropriate configuration value.
-
-	while((c = getopt(argc,argv,"x:y:s:W:H:m:o:n:h"))!=-1) {
-		switch(c) 
-		{
-			case 'x':
-				xcenter = atof(optarg);
-				break;
-			case 'y':
-				ycenter = atof(optarg);
-				break;
-			case 's':
-				xscale = atof(optarg);
-				break;
-			case 'W':
-				image_width = atoi(optarg);
-				break;
-			case 'H':
-				image_height = atoi(optarg);
-				break;
-			case 'm':
-				max = atoi(optarg);
-				break;
-			case 'o':
-				outfile = optarg;
-				break;
-			case 'n':
-				number = atoi(optarg);
-            	break;
-			case 'h':
-				show_help();
-				exit(1);
-				break;
-		}
-	}		
-	double chunkSize = (workload / number);  // number of items each child proecesses
-	float remainder = workload % number;    // remainder if workload doesnt divide evenly
-	//int work = ceiling(50.0/children)
-	// add check if current image over or = to 50, exit
-	int imageNumber;
-
-	for (int i = 0; i<50; i++){
-        pid_t pid = fork();
-		scale = scale * .97;
-			if (pid==-1){
-            	perror("fork failed");
-            	exit(EXIT_FAILURE);
-        	} else if (pid == 0){
-				for(imageNumber = 0; imageNumber < chunkSize; imageNumber++){
-					sprintf(newName, outfile,i);
-					// Calculate y scale based on x scale (settable) and image sizes in X and Y (settable)
-					yscale  = (xscale*scale) / image_width * image_height;
-					// Display the configuration of the image.
-					printf("mandel: x=%lf y=%lf xscale=%lf yscale=%1f max=%d outfile=%s\n",xcenter,ycenter,xscale,yscale,max,newName);
-					// Create a raw image of the appropriate size.
-					imgRawImage* img = initRawImage(image_width,image_height);
-					// Fill it with a black
-					setImageCOLOR(img,0);
-					// Compute the Mandelbrot image
-					compute_image(img,xcenter-xscale/2,xcenter+xscale/2,ycenter-yscale/2,ycenter+yscale/2,max);
-					// Save the image in the stated file.
-					storeJpegImageFile(img,newName);
-					// free the mallocs
-					freeRawImage(img);
-					sleep(2);
-					exit(0);
-				}
-			}
-		}
-
-	return 0;
+void *thread_compute(void *arg) {
+    ThreadJob *job = (ThreadJob *)arg;
+    
+    int width  = job->img->width;
+    int height = job->img->height;
+    
+    for (int j = job->startRow; j < job->endRow; j++) {
+        for (int i = 0; i < width; i++) {
+            double x = job->xmin + i * (job->xmax - job->xmin) / width;
+            double y = job->ymin + j * (job->ymax - job->ymin) / height;
+            
+            int iters = iterations_at_point(x, y, job->maxIter);
+            unsigned int color = (0xFFFFFF * iters) / job->maxIter;
+            
+            setPixelCOLOR(job->img, i, j, color);
+        }
+    }
+    
+    return NULL;
 }
-/*
-Return the number of iterations at point x, y
-in the Mandelbrot space, up to a maximum of max.
-*/
-int iterations_at_point( double x, double y, int max )
-{
-	double x0 = x;
-	double y0 = y;
 
-	int iter = 0;
-
-	while( (x*x + y*y <= 4) && iter < max ) {
-
-		double xt = x*x - y*y + x0;
-		double yt = 2*x*y + y0;
-
-		x = xt;
-		y = yt;
-
-		iter++;
-	}
-
-	return iter;
-}
 /*
 Compute an entire Mandelbrot image, writing each point to the given bitmap.
 Scale the image to the range (xmin-xmax,ymin-ymax), limiting iterations to "max"
 */
-void compute_image(imgRawImage* img, double xmin, double xmax, double ymin, double ymax, int max )
-{
-	int i,j;
-
-	int width = img->width;
-	int height = img->height;
-
-	// For every pixel in the image...
-
-	for(j=0;j<height;j++) {
-
-		for(i=0;i<width;i++) {
-
-			// Determine the point in x,y space for that pixel.
-			double x = xmin + i*(xmax-xmin)/width;
-			double y = ymin + j*(ymax-ymin)/height;
-
-			// Compute the iterations at that point.
-			int iters = iterations_at_point(x,y,max);
-
-			// Set the pixel in the bitmap.
-			setPixelCOLOR(img,i,j,iteration_to_color(iters,max));
-		}
-	}
+void compute_image(imgRawImage *bm, double xmin, double xmax, double ymin, double ymax, int maxIter, int numThreads) {
+    int height = bm->height;
+    
+    // Handle single thread case
+    if (numThreads == 1) {
+        int width = bm->width;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                double x = xmin + i * (xmax - xmin) / width;
+                double y = ymin + j * (ymax - ymin) / height;
+                int iters = iterations_at_point(x, y, maxIter);
+                unsigned int color = (0xFFFFFF * iters) / maxIter;
+                setPixelCOLOR(bm, i, j, color);
+            }
+        }
+        return;
+    }
+    
+    // multithreads
+    pthread_t threads[numThreads];
+    ThreadJob jobs[numThreads];
+    int rowsPerThread = height / numThreads;
+    int remainder = height % numThreads;
+    for (int t = 0; t < numThreads; t++) {
+        jobs[t].img = bm;
+        jobs[t].xmin = xmin;
+        jobs[t].xmax = xmax;
+        jobs[t].ymin = ymin;
+        jobs[t].ymax = ymax;
+        jobs[t].maxIter = maxIter;
+        jobs[t].startRow = t * rowsPerThread;
+        jobs[t].endRow = (t + 1) * rowsPerThread;
+        
+        // Distribute remainder to the last thread
+        if (t == numThreads - 1) {
+            jobs[t].endRow = height;
+        }
+        
+        int ret = pthread_create(&threads[t], NULL, thread_compute, &jobs[t]);
+        if (ret != 0) {
+            fprintf(stderr, "Error creating thread %d\n", t);
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    // wait until finished
+    for (int t = 0; t < numThreads; t++) {
+        pthread_join(threads[t], NULL);
+    }
 }
 
+int main( int argc, char *argv[] )
+{
+    char c;
+    const char *outfile = "mandel2%d.jpg"; // changed to mandel 2 because whenever i delete the first ones from last lab they appear again
+    char newName[50];
+    double xcenter = 0;
+    double ycenter = 0;
+    double xscale = 4;
+    double yscale = 0; // will be calculated for each image
+    int    image_width = 1000;
+    int    image_height = 1000;
+    int    max = 1000;
+    
+
+    
+    while((c = getopt(argc,argv,"x:y:s:W:H:m:o:n:t:z:h"))!=-1) {
+        switch(c) 
+        {
+            case 'x':
+                xcenter = atof(optarg);
+                break;
+            case 'y':
+                ycenter = atof(optarg);
+                break;
+            case 's':
+                xscale = atof(optarg);
+                break;
+            case 'W':
+                image_width = atoi(optarg);
+                break;
+            case 'H':
+                image_height = atoi(optarg);
+                break;
+            case 'm':
+                max = atoi(optarg);
+                break;
+            case 'o':
+                outfile = optarg;
+                break;
+            case 'n':
+                number = atoi(optarg);
+                if (number < 1) number = 1;
+                break;
+            case 't':
+                numThreads = atoi(optarg);
+                if (numThreads < 1) numThreads = 1;
+                if (numThreads > 20) numThreads = 20;
+                printf("Using %d thread(s) per process\n", numThreads);
+                break;
+            case 'z':  // zoom factor to test if its actually creating different images
+                zoom_factor = atof(optarg);
+                if (zoom_factor <= 0 || zoom_factor >= 1) {
+                    fprintf(stderr, "Zoom factor must be between 0 and 1\n");
+                    zoom_factor = 0.98;
+                }
+                break;
+            case 'h':
+                show_help();
+                exit(1);
+                break;
+        }
+    }
+    
+    int totalImages = 50;
+    int chunkSize = totalImages / number;
+    
+    pid_t pid;
+    int p;
+    
+    for (p = 0; p < number; p++) {
+        pid = fork();
+        
+        if (pid == -1) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        }
+        
+        if (pid == 0) {
+            break;
+        }
+    }
+
+    // child process
+    if (pid == 0) {
+
+        // Each child gets chunkSize images
+        for (int imageNumber = 0; imageNumber < chunkSize; imageNumber++) {
+            int globalIndex = p * chunkSize + imageNumber;
+            sprintf(newName, outfile, globalIndex);
+            
+            // Apply zoom scale for each image
+            double current_xscale = xscale * pow(zoom_factor, globalIndex);
+            double current_yscale = (current_xscale * image_height) / image_width;
+            
+            imgRawImage *img = initRawImage(image_width, image_height);
+            setImageCOLOR(img, 0);
+            
+            compute_image(img, xcenter - current_xscale / 2, xcenter + current_xscale / 2, 
+				ycenter - current_yscale / 2, ycenter + current_yscale / 2,
+                max, numThreads);
+            
+            storeJpegImageFile(img, newName);
+            freeRawImage(img);
+            
+        }
+        
+        exit(0); // child done
+    }
+
+    // parent waits for children
+    if (pid > 0) {
+        for (int i = 0; i < number; i++) {
+            wait(NULL);
+		}
+    }
+    
+    return 0;
+}
+
+/*
+Return the number of iterations at point x, y
+in the Mandelbrot space, up to a maximum of max.
+*/
+int iterations_at_point( double x, double y, int max ) {
+    double x0 = x;
+    double y0 = y;
+    
+    int iter = 0;
+    
+    while( (x*x + y*y <= 4) && iter < max ) {
+        double xt = x*x - y*y + x0;
+        double yt = 2*x*y + y0;
+        
+        x = xt;
+        y = yt;
+        
+        iter++;
+    }
+    
+    return iter;
+}
 
 /*
 Convert a iteration number to a color.
 Here, we just scale to gray with a maximum of imax.
 Modify this function to make more interesting colors.
 */
-int iteration_to_color( int iters, int max )
-{
-	int color = 0xFFFFFF*iters/(double)max;
-	return color;
+int iteration_to_color( int iters, int max ) {
+    int color = 0xFFFFFF * iters / (double)max;
+    return color;
 }
 
-
 // Show help message
-void show_help()
-{
-	printf("Use: mandel [options]\n");
-	printf("Where options are:\n");
-	printf("-m <max>    The maximum number of iterations per point. (default=1000)\n");
-	printf("-x <coord>  X coordinate of image center point. (default=0)\n");
-	printf("-y <coord>  Y coordinate of image center point. (default=0)\n");
-	printf("-s <scale>  Scale of the image in Mandlebrot coordinates (X-axis). (default=4)\n");
-	printf("-W <pixels> Width of the image in pixels. (default=1000)\n");
-	printf("-H <pixels> Height of the image in pixels. (default=1000)\n");
-	printf("-o <file>   Set output file. (default=mandel.bmp)\n");
-	printf("-h          Show this help text.\n");
-	printf("\nSome examples are:\n");
-	printf("mandel -x -0.5 -y -0.5 -s 0.2\n");
-	printf("mandel -x -.38 -y -.665 -s .05 -m 100\n");
-	printf("mandel -x 0.286932 -y 0.014287 -s .0005 -m 1000\n\n");
+void show_help() {
+    printf("Use: mandel [options]\n");
+    printf("Where options are:\n");
+    printf("-m <max>    The maximum number of iterations per point. (default=1000)\n");
+    printf("-x <coord>  X coordinate of image center point. (default=0)\n");
+    printf("-y <coord>  Y coordinate of image center point. (default=0)\n");
+    printf("-s <scale>  Initial scale of the image (X-axis). (default=4)\n");
+    printf("-W <pixels> Width of the image in pixels. (default=1000)\n");
+    printf("-H <pixels> Height of the image in pixels. (default=1000)\n");
+    printf("-o <file>   Set output file pattern with %%d for frame number. (default=mandel%%d.jpg)\n");
+    printf("-n <num>    Number of processes. (default=1)\n");
+    printf("-t <num>    Number of threads per process (1-20). (default=1)\n");
+    printf("-z <factor> Zoom factor per frame (0 < factor < 1). (default=0.98)\n");
+    printf("-h          Show this help text.\n");
+    printf("\nExamples:\n");
+    printf("mandel -x -0.5 -y -0.5 -s 4 -n 2 -t 4 -z 0.95\n");
+    printf("  Generate 50-frame zoom movie with 2 processes, 4 threads each, 0.95 zoom factor\n");
+    printf("\nTo create a movie from generated frames:\n");
+    printf("ffmpeg -framerate 30 -i mandel%%d.jpg -c:v libx264 -pix_fmt yuv420p mandel_movie.mp4\n\n");
 }
